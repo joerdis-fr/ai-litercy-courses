312237a0d5009b3d261e3b3a3b99ef61ab4d10e2
Joerdis Friedrichsen <jn.friedrichsen@stud.dhsh.de>
2026-06-03 17:23:38 +0200

    Header eingebaut

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
+<script lang="ts" setup>
+import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/Header.vue'
+</script>
+
 <template>
   <v-app>
     <v-main>
+      <Header />
       <HelloWorld />
     </v-main>
-    <v-btn
-      class="ma-2"
-      icon="mdi-theme-light-dark"
-      location="top right"
-      position="absolute"
-      @click="$vuetify.theme.cycle()"
-    />
+<!--    <v-btn-->
+<!--      class="ma-2"-->
+<!--      icon="mdi-theme-light-dark"-->
+<!--      location="top right"-->
+<!--      position="absolute"-->
+<!--      @click="$vuetify.theme.cycle()"-->
+<!--    />-->
   </v-app>
 </template>
-
-<script lang="ts" setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
-</script>
