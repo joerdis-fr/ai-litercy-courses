12abe2398846473b9f770cc5849dd5cd9651f6a1
Joerdis Friedrichsen <jn.friedrichsen@stud.dhsh.de>
2026-06-13 12:10:50 +0200

    Bessere Anzeige

--- src/components/KursKarteAusgeklappt.vue
+++ src/components/KursKarteAusgeklappt.vue
@@ -13,27 +13,37 @@ const props = defineProps<{
 }>();
 
 const kurs = props.course
-const paper = props.course.paper || null
-const tool = props.course.tool
+const paper = kurs.paper || null
+const tool = kurs.tool
 
+// Falls der Kurslink fehlt
 const kursDisabled = () => {
   return !kurs.tool.link;
 }
-
-const paperDisabled = () => {
-  return !paper;
-}
-
 const kursLink = () => {
   if (tool.link)
     return tool.link;
   else return "#"
 }
 
+// Falls das Paper fehlt
+const paperDisabled = () => {
+  return !paper;
+}
 const paperLink = () => {
   if (paper)
     return paper.link;
   else return "#"
+}
+const laender = () => {
+  if (paper)
+    return paper.land;
+  else return [];
+}
+const forschungsfragen = () => {
+  if (paper)
+    return paper.forschungsfrage;
+  else return [];
 }
 
 </script>
@@ -48,24 +58,46 @@ const paperLink = () => {
       <ul>
         <li>Zielgruppe: {{kurs.zielgruppe}}</li>
         <li>Kategorie: {{kurs.kategorie[0]}}</li>
-        <li>Länge: {{ kurs.laenge?.anzahlSessions }} x {{ kurs.laenge?.zeitInMinutes }} m</li>
+        <li>Länge: {{ kurs.laenge?.anzahlSessions }}x {{ kurs.laenge?.zeitInMinutes }} m</li>
       </ul>
       <div v-if="paper != null">
         <v-divider />
         <h4>Infos zum zugehörigen Paper</h4>
         <h3>{{ paper.titel }}</h3>
+        <p class="text-body-medium text-grey-darken-1 mt-n4"> {{ paper.veroeffentlichungsdatum }} </p>
         <ul>
           <li>Bemerkung: {{ paper.bemerkung }}</li>
-          <li>Veröffentlichungsdatum: {{ paper.veroeffentlichungsdatum }}</li>
           <li>Methode zur Datenerhebung: {{ paper.methode }}</li>
           <li>Sample: {{ paper.sample }}</li>
           <li>Ergebnisse: {{ paper.ergebnisse }}</li>
           <li>von {{ paper.datenbank }}</li>
-
-          <li>Länder: {{paper.land[0]}}</li>
-          <li>Forschungsfrage(n): {{paper.forschungsfrage[0]}}</li>
+          <li
+              v-if="laender().length == 1"
+          >
+            Land: {{paper.land[0]}}
+          </li>
+          <li
+              v-if="forschungsfragen().length == 1"
+          >
+            Forschungsfrage: {{forschungsfragen()[0]}}</li>
         </ul>
-
+        <div v-if="laender().length > 1" class="ml-6">
+        Länder:
+        <ul>
+          <li v-for="(land, index) in laender()" :key="index">
+            {{ land }}
+          </li>
+        </ul>
+        </div>
+        <div v-if="forschungsfragen().length > 1" class="ml-6">
+          Forschungsfragen:
+          <ul>
+            <li v-for="(forschungsfrage, index) in forschungsfragen()" :key="index">
+              {{ forschungsfrage }}
+            </li>
+          </ul>
+        </div>
+        <v-divider />
       </div>
     </template>
       <template v-slot:actions>
